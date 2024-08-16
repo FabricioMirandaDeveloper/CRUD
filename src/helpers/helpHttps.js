8237d9db3d1b184d0958bf9c016d3686b09044c9
Fabricio Miranda <mirandaquezadafabriciojoel@gmail.com>
2024-08-16 17:07:26 -0500

    en proceso la contruccion de mi helphttps 2.0

--- src/helpers/helpHttps.js
+++ src/helpers/helpHttps.js
@@ -1,30 +1,27 @@
 export const helpHttps = () => {
-    const customFetch = (endpoint, options) = {
+    const customFetch = (endpoint, options) => {
         const defaultHeader = {
-            accept: "application/json"
-        }
-    }
-    const controller = new AbortController()
-    options.signal = controller.signal
+            accept: "application/json",
+        };
+        const controller = new AbortController();
+        options.signal = controller.signal;
 
-    options.method = options.method || "GET"
-    options.headers = options.headers
-    ? {...defaultHeader, ...options.body}
-    : defaultHeader
-}
+        options.method = options.method || "GET";
+        options.headers = options.headers
+            ? { ...defaultHeader, ...options.body }
+            : defaultHeader;
 
-const get = () => {
+        options.body = JSON.stringify(options.body) || false;
+        if (!options.body) delete options.body;
 
-}
+        console.log(options)
+    };
+};
 
-const post = () => {
+const get = () => { };
 
-}
+const post = () => { };
 
-const put = () => {
+const put = () => { };
 
-}
-
-const del = () => {
-
-}
+const del = () => { };
